a4ae38eb2bf60e8f9ef41013221477aee274feec
Aloth1 <lexlutin@gmail.com>
2017-06-08 15:45:06 +0300

    Update 2.C

--- C/2.C
+++ C/2.C
@@ -1,1 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int day = 30;
+	int month = 1;
+	int d = day;
+	printf("%i.0%i\n", day, month);/*Обозначению формата предшествует знак процента (%) %i – вывод значения как целого со знаком */
+	day = month;
+	month = d;
+	printf("0%i.%i", day, month);
+	getchar();
+	return 0;
+}
 
