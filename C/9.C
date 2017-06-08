1ebdc27cf544d0ad3534daa6d2ccde30b8a05249
Aloth1 <lexlutin@gmail.com>
2017-06-08 15:48:11 +0300

    Create 9.C

---
+++ C/9.C
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int fib(int n)
+{
+       if (n == 0){
+        return 0;
+    }
+    else if(n == 1){
+        return 1;
+    }
+    else {
+        return fib(n-1)+fib(n-2);
+    }
+}
+
+int main()
+{
+    setlocale(0, "russian");
+    int n = 0;
+    int i = 0;
+    printf("Введите N\n");
+    scanf("%i", &n);
+    for (i=0;i<=n;++i){
+        printf("%i|%i\n", i, fib(i));
+    }
+    return 0;
+}
